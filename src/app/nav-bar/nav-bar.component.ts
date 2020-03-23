668644d89a163cc405857dcbfa36bb3b32f99846
Andrew Moris <Andrew.Moris@vodafone.com>
2020-03-23 12:13:50 +0200

    updating login and logout links

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLoginClick() {
+
+  }
+
+  onLogoutClick() {
+
   }
 
 }
