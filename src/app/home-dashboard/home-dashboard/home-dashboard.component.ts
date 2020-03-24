fdec3b6d8bb6cdc63d1f5c071550bafe745cfaa2
Andrew Moris <Andrew.Moris@vodafone.com>
2020-03-24 11:22:36 +0200

    fixing landing page missing images

--- src/app/home-dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/home-dashboard/home-dashboard/home-dashboard.component.ts
@@ -9,7 +9,7 @@ export class HomeDashboardComponent implements OnInit {
 
   // tslint:disable-next-line: max-line-length
   descriptionSection1 = 'Reduce the effort done in sizing using the latest technologies to implement a robust solution to minimize the effort.';
-  imgSection1 = '../../../assets/images/graphic-1714230.svg';
+  imgSection1 = '../../../assets/images/agenda-153555.svg';
 
   // tslint:disable-next-line: max-line-length
   descriptionSection2 = 'View statistics from the statring date of writing a PDD/SDD to the time to makrket date to provide govrnance on the bot development process.';
